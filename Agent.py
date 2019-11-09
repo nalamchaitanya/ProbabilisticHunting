93d7f2909a6d521fa6491225e5a9b3e913e5c5db
ShreyansGupta <50500026+ShreyansGupta@users.noreply.github.com>
2019-11-09 17:09:28 -0500

    First Commit
    ChooseNextCell and _init_

--- Agent.py
+++ Agent.py
@@ -4,11 +4,19 @@ import numpy as np
 import random
 import copy
 import pdb
+import numpy as np
 # 
 class Agent:
     def __init__(self,env):
+        # Initialize the Agent Probability Hunting Matrix using the dimensions of environment
+        self.dimension=env.dimension
+        self.probLandscape= np.zeros((self.dimension,self.dimension))
 
-    # This method returns unrevealed neighbors of a cell,total safe neighbors and revealed mines
+
     def chooseNextCell(self):
+        # This function returns coordinates of cell having Max Probability
+        result = np.where(self.probLandscape == np.amax(self.probLandscape))
+        list_index=list(zip(result[0],result[1]))
+        return list_index[0]
 
     def updateProbability(self, row, column):
