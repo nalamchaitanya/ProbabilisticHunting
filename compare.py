4c19fac7c71ddf1a1e144697de0a9420e0da8aaf
Chaitanya <nalamchaitanya@gmail.com>
2019-11-12 12:40:00 -0500

    Modified compare.py

--- compare.py
+++ compare.py
@@ -4,20 +4,25 @@ import strategy
 import random
 import copy
 
-environ = Environment.Environment(50)
-isInCellcount = 0
-isFoundInCellcount = 0
-iterations = 1000
-for i in range(iterations):
-	environ.count = 0
-	environ.target = (int(random.uniform(0,environ.dimension-1)), int(random.uniform(0,environ.dimension-1)))
-	agent = Agent.Agent(environ)
-	agent2 = copy.deepcopy(agent)
-	count1 = agent.getSearchCount(strategy.isInCell)
-	count2 = agent2.getSearchCount(strategy.isFoundInCell)
-	print(str(count1)+" "+str(count2))
-	isInCellcount += count1
-	isFoundInCellcount += count2
+iterations = 100
+inCellCountList = []
+foundInCellCountList = []
+for d in range(40,70,10):
+	environ = Environment.Environment(d)
+	isInCellcount = 0
+	isFoundInCellcount = 0
+	for i in range(iterations):
+		environ.count = 0
+		environ.target = (int(random.uniform(0,environ.dimension-1)), int(random.uniform(0,environ.dimension-1)))
+		agent = Agent.Agent(environ)
+		agent2 = copy.deepcopy(agent)
+		count1 = agent.getSearchCount(strategy.isInCell)
+		count2 = agent2.getSearchCount(strategy.isFoundInCell)
+		print(str(count1)+" "+str(count2))
+		isInCellcount += count1
+		isFoundInCellcount += count2
+	inCellCountList.append(float(isInCellcount)/(iterations*d*d));
+	foundInCellCountList.append(float(isFoundInCellcount)/iterations*d*d);
 
-print ("Average count for isInCell : "+str(isInCellcount/100))
-print ("Average count for isFoundInCell : "+str(isFoundInCellcount/100))
+print ("Average count for isInCell : "+str(inCellCountList))
+print ("Average count for isFoundInCell : "+str(foundInCellCountList))
