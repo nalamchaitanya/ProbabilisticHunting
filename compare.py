be084b0e3c5d43410a02a9aa5e92585cad5f5e83
Chaitanya <nalamchaitanya@gmail.com>
2019-11-17 23:17:01 -0500

    Updated compare.py

--- compare.py
+++ compare.py
@@ -11,39 +11,49 @@ from collections import Counter
 iterations = 50
 median1List = []
 median2List = []
+median3List = []
 mean1List = []
 mean2List = []
-mode1List = []
-mode2List = []
-for d in range(20,110,10):
+mean3List = []
+# mode1List = []
+# mode2List = []
+for d in range(20,100,10):
 	environ = Environment.Environment(d)
-	isInCellcount = []
-	isFoundInCellcount = []
+	firstStrategy = []
+	secondStrategy = []
+	thirdStrategy = []
 	for i in range(iterations):
 		environ.count = 0
 		environ.target = (int(random.uniform(0,environ.dimension-1)), int(random.uniform(0,environ.dimension-1)))
-		agent = Agent.Agent(environ)
-		# agent2 = copy.deepcopy(agent)
-		agent3 = copy.deepcopy(agent)
-		count1 = agent.getSearchCount(strategy.isInCell, strategy.manhattan)
+		agent1 = Agent.Agent(environ)
+		agent2 = copy.deepcopy(agent1)
+		agent3 = copy.deepcopy(agent1)
+		# count1 = agent.getSearchCount(strategy.isInCell, strategy.manhattan)
 		# count2 = agent2.getSearchCount(strategy.isFoundInCell, strategy.one)
-		count2 = agent3.getSearchCount(strategy.maxPair, strategy.manhattan)
-		print(str(count1)+" "+str(count2))
-		isInCellcount.append(count1)
-		isFoundInCellcount.append(count2)
+		count1 = agent1.getSearchCount(strategy.maxPair, strategy.manhattan)
+		count2 = agent2.getSearchCount(strategy.maxPair, strategy.manhattan, True)
+		count3 = agent3.getSearchCount(strategy.maxPairMovingTarget, strategy.manhattan, True)
+		print(str(count1)+" "+str(count2)+" "+str(count3))
+		firstStrategy.append(count1)
+		secondStrategy.append(count2)
+		thirdStrategy.append(count3)
 	# Do median and mode and etc caclculation here.
 	print("d : "+str(d))
-	isInCellcount.sort()
-	median1 = statistics.median(isInCellcount)#[len(isInCellcount)/2]
-	mean1 = float(sum(isInCellcount[iterations//4:3*iterations//4]))/(iterations/2)
-	isFoundInCellcount.sort()
-	median2 = statistics.median(isFoundInCellcount)#[len(isFoundInCellcount)/2]
-	mean2 = float(sum(isFoundInCellcount[iterations//4:3*iterations//4]))/(iterations/2)
+	firstStrategy.sort()
+	median1 = statistics.median(firstStrategy)#[len(isInCellcount)/2]
+	mean1 = float(sum(firstStrategy[iterations//4:3*iterations//4]))/(iterations/2)
+	secondStrategy.sort()
+	median2 = statistics.median(secondStrategy)#[len(isFoundInCellcount)/2]
+	mean2 = float(sum(secondStrategy[iterations//4:3*iterations//4]))/(iterations/2)
+	thirdStrategy.sort()
+	median3 = statistics.median(thirdStrategy)
+	mean3 = float(sum(thirdStrategy[iterations//4:3*iterations//4]))/(iterations/2)
 	mean1List.append(float(mean1)/(d*d))
 	mean2List.append(float(mean2)/(d*d))
+	mean3List.append(float(mean3)/(d*d))
 	median1List.append(float(median1)/(d*d))
 	median2List.append(float(median2)/(d*d))
-
+	median3List.append(float(median3)/(d*d))
 	# isInCellcount1 = [float(x)/(d*d) for x in isInCellcount]
 	# isInCellcountBin = pd.cut(isInCellcount1, [0,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9])
 	# isFoundInCellcount1 = [float(x)/(d*d) for x in isFoundInCellcount]
@@ -60,8 +70,11 @@ for d in range(20,110,10):
 
 print('mean1: '+str(mean1List))
 print('mean2: '+str(mean2List))
+print('mean3: '+str(mean3List))
 print('median1: '+str(median1List))
 print('median2: '+str(median2List))
+print('median3: '+str(median3List))
+
 # print('mode1: '+str(mode1List))
 # print('mode2: '+str(mode2List))
 
@@ -69,24 +82,36 @@ print('median2: '+str(median2List))
 
 
 fig = plt.figure()
-dimensionList = range(20,110,10)
+dimensionList = range(20,100,10)
 plt.plot(dimensionList, median1List,label="Rule 1")
-plt.plot(dimensionList, median2List, label="Rule 2")
+plt.plot(dimensionList, median2List, label="Target Moves")
+plt.plot(dimensionList, median3List, label="Terrain Type")
 plt.xlabel('Dimension')
 plt.ylabel('Median of action count / No of cells')
-plt.title('Rule 1 vs Utility')
+plt.title('Median')
 plt.legend()
 plt.show()
 
-fig2 = plt.figure()
-# dimensionList = range(20,80,10)
+fig = plt.figure()
+dimensionList = range(20,100,10)
 plt.plot(dimensionList, mean1List,label="Rule 1")
-plt.plot(dimensionList, mean2List, label="Utility")
+plt.plot(dimensionList, mean2List, label="Target Moves")
+plt.plot(dimensionList, mean3List, label="Terrain Type")
 plt.xlabel('Dimension')
 plt.ylabel('Mean of action count / No of cells')
-plt.title('Rule 1 vs Utility')
+plt.title('Mean')
 plt.legend()
 plt.show()
+
+# fig2 = plt.figure()
+# # dimensionList = range(20,80,10)
+# plt.plot(dimensionList, mean1List,label="Rule 1")
+# plt.plot(dimensionList, mean2List, label="Utility")
+# plt.xlabel('Dimension')
+# plt.ylabel('Mean of action count / No of cells')
+# plt.title('Rule 1 vs Utility')
+# plt.legend()
+# plt.show()
 
 # print ("Average count for isInCell : "+str(inCellCountList))
 # print ("Average count for isFoundInCell : "+str(foundInCellCountList))
--- test.py
+++ test.py
@@ -5,7 +5,9 @@ import copy
 
 environ = Environment.Environment(50)
 agent = Agent.Agent(environ)
-count = agent.getSearchCount(strategy.maxPair, strategy.manhattan, True)
 agent2 = copy.deepcopy(agent)
-count2 = agent.getSearchCount(strategy.maxPairMovingTarget, strategy.manhattan, True)
-print(str(count)+" "+str(count2))
+agent3 = copy.deepcopy(agent)
+count3 = agent3.getSearchCount(strategy.maxPair, strategy.manhattan, False)
+count = agent.getSearchCount(strategy.maxPair, strategy.manhattan, True)
+count2 = agent2.getSearchCount(strategy.maxPairMovingTarget, strategy.manhattan, True)
+print(str(count3)+" "+str(count)+" "+str(count2))
